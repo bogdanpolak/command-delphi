5f2573d6c7f77a1c0ed16717ef91cbcae0bcf484
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-08-28 10:43:55 +0200

    [tests] Prepared command factory env

--- tests/Tests.TCommandFactory.pas
+++ tests/Tests.TCommandFactory.pas
@@ -9,10 +9,10 @@ uses
 {$M+}
 
 type
-
   [TestFixture]
-  TObervableTests = class(TObject)
-  private
+  TCommandFactoryTests = class(TObject)
+  strict private
+    FStrings: TStringList;
   public
     [Setup]
     procedure Setup;
@@ -23,21 +23,78 @@ type
     procedure TestSample1;
   end;
 
+type
+  TCommandA = class (TCommand)
+  strict private
+    FActive: boolean;
+    FCount: integer;
+  protected
+    procedure Guard; override;
+  public
+    procedure Execute; override;
+    property Active: boolean read FActive write FActive;
+    property Count: integer read FCount write FCount;
+  end;
+
+  TCommandStringList = class (TCommand)
+  strict private
+    FCount: integer;
+    FLines: TStringList;
+  protected
+    procedure Guard; override;
+  public
+    procedure Execute; override;
+    property Count: integer read FCount write FCount;
+    property Lines: TStringList read FLines write FLines;
+  end;
+
+
 implementation
 
+// ------------------------------------------------------------------------
+// class TCommandA
+// ------------------------------------------------------------------------
+{$REGION 'class TCommandA'}
+procedure TCommandA.Guard; 
+begin
+end;
+
+procedure TCommandA.Execute;
+begin
+  Active := True;
+  Count := Count +1;
+end;
+
+{$ENDREGION}
+// ------------------------------------------------------------------------
+// class TCommandStringList
+// ------------------------------------------------------------------------
+{$REGION 'class TCommandStringList'}
+procedure TCommandStringList.Guard; 
+begin
+  Assert(Lines<>nil);
+end;
+
+procedure TCommandStringList.Execute;
+begin
+  Count := Count +1;
+  Lines.Add (Format('%.3d',[Count]));
+end;
+
+{$ENDREGION}
 // ------------------------------------------------------------------------
 // Setup and TearDown
 // ------------------------------------------------------------------------
 {$REGION 'Setup and tear down'}
 
-procedure TObervableTests.Setup;
+procedure TCommandFactoryTests.Setup;
 begin
-
+  FStrings := TStringList.Create();
 end;
 
-procedure TObervableTests.TearDown;
+procedure TCommandFactoryTests.TearDown;
 begin
-
+  FreeAndNil (FStrings);
 end;
 
 {$ENDREGION}
@@ -46,7 +103,7 @@ end;
 // ------------------------------------------------------------------------
 {$REGION 'Basic tests'}
 
-procedure TObervableTests.TestSample1;
+procedure TCommandFactoryTests.TestSample1;
 begin
 
 end;
