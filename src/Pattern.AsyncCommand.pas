861c2c2e5a05d335c498d06351ddafde3dadf59e
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-02-06 22:22:52 +0000

    Separated TAsyncCommand

---
+++ src/Pattern.AsyncCommand.pas
@@ -0,0 +1,75 @@
+﻿{* ------------------------------------------------------------------------ *
+ * Command Parttern  ♥  TAsyncCommand
+ * ------------------------------------------------------------------------ *}
+unit Pattern.AsyncCommand;
+
+interface
+
+uses
+  System.Classes,
+  System.SysUtils,
+  System.TypInfo,
+
+  Pattern.Command;
+
+type
+  TAsyncCommand = class(TCommand)
+  private const
+    Version = '0.7';
+  protected
+    fThread: TThread;
+    fIsThreadTermianed: boolean;
+    procedure DoPrepare; virtual; abstract;
+    procedure DoTeardown; virtual; abstract;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Execute; override;
+    function IsFinished: boolean;
+  end;
+
+implementation
+
+// ------------------------------------------------------------------------
+// TAsyncCommand
+// ------------------------------------------------------------------------
+
+constructor TAsyncCommand.Create(AOwner: TComponent);
+begin
+  inherited;
+  fThread := nil;
+  fIsThreadTermianed := true;
+end;
+
+procedure TAsyncCommand.Execute;
+begin
+  DoGuard;
+  DoPrepare;
+  fThread := TThread.CreateAnonymousThread(
+    procedure
+    begin
+      try
+        fIsThreadTermianed := False;
+        DoExecute;
+      finally
+        // TODO: lock or critical section is required bellow (critical !!!)
+        fIsThreadTermianed := true;
+      end;
+    end);
+  fThread.FreeOnTerminate := False;
+  fThread.Start;
+end;
+
+function TAsyncCommand.IsFinished: boolean;
+begin
+  if fThread = nil then
+    Exit(true);
+  Result := fIsThreadTermianed;
+  if Result and (fThread <> nil) then
+  begin
+    fThread.Free;
+    fThread := nil;
+    DoTeardown;
+  end;
+end;
+
+end.
--- src/Pattern.Command.pas
+++ src/Pattern.Command.pas
@@ -29,17 +29,6 @@ type
     procedure Execute; virtual;
   end;
 
-  TAsyncCommand = class(TCommand)
-  protected
-    fThread: TThread;
-    fIsThreadTermianed: boolean;
-    procedure DoPrepare; virtual; abstract;
-    procedure DoTeardown; virtual; abstract;
-  public
-    constructor Create(AOwner: TComponent); override;
-    procedure Execute; override;
-    function IsFinished: boolean;
-  end;
 
   TPropertyInfo = record
     Kind: TTypeKind;
@@ -115,49 +104,6 @@ begin
   end;
 end;
 
-
-// ------------------------------------------------------------------------
-// TAsyncCommand
-// ------------------------------------------------------------------------
-
-constructor TAsyncCommand.Create(AOwner: TComponent);
-begin
-  inherited;
-  fThread := nil;
-  fIsThreadTermianed := true;
-end;
-
-procedure TAsyncCommand.Execute;
-begin
-  DoGuard;
-  DoPrepare;
-  fThread := TThread.CreateAnonymousThread(
-    procedure
-    begin
-      try
-        fIsThreadTermianed := False;
-        DoExecute;
-      finally
-        // TODO: lock or critical section is required bellow (critical !!!)
-        fIsThreadTermianed := true;
-      end;
-    end);
-  fThread.FreeOnTerminate := False;
-  fThread.Start;
-end;
-
-function TAsyncCommand.IsFinished: boolean;
-begin
-  if fThread = nil then
-    Exit(true);
-  Result := fIsThreadTermianed;
-  if Result and (fThread <> nil) then
-  begin
-    fThread.Free;
-    fThread := nil;
-    DoTeardown;
-  end;
-end;
 
 // ------------------------------------------------------------------------
 // TComponentInjector
